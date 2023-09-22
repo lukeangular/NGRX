109f401fb136c04411f89319a04c1b10ef16aa9b
sundartamang <sundartamang6@gmail.com>
2023-09-22 15:16:37 +0545

    feat:dev tools done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter/counter.component';
 import { CouterOutputComponent } from './counter/couter-output/couter-output.component';
 import { CouterButtonsComponent } from './counter/couter-buttons/couter-buttons.component';
-import {StoreModule} from '@ngrx/store'
+import { StoreModule } from '@ngrx/store'
 import { counterReducer } from './counter/satate/counter.reducer';
 import { CustomCounterInputComponent } from './counter/custom-counter-input/custom-counter-input.component';
 import { FormsModule } from '@angular/forms';
@@ -13,6 +13,8 @@ import { HomeComponent } from './home/home.component'
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { PostsListComponent } from './posts/posts-list/posts-list.component';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from './environments/environment';
 
 @NgModule({
   declarations: [
@@ -29,7 +31,13 @@ import { PostsListComponent } from './posts/posts-list/posts-list.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    StoreModule.forRoot({counter: counterReducer})
+    StoreModule.forRoot({ counter: counterReducer }),
+    StoreDevtoolsModule.instrument(
+      {
+        maxAge: 25, // number of state it could show (last 25)
+        logOnly: environment.production
+      }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
