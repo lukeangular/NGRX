d1600d28005a1154ba196eeaff835d6fe40302d2
sundartamang <sundartamang6@gmail.com>
2023-09-23 19:14:27 +0545

    feat:auth module done

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from "@angular/core";
+import { LoginComponent } from './login/login.component';
+import { RouterModule, Routes } from "@angular/router";
+import { CommonModule } from "@angular/common";
+import { ReactiveFormsModule } from "@angular/forms";
+
+const routes: Routes = [
+    {
+        path: '',
+        children: [
+            { path: '', redirectTo: 'login', pathMatch:'full' },
+            { path: 'login', component: LoginComponent }
+        ]
+    }
+]
+@NgModule({
+    declarations: [
+        LoginComponent
+    ],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        RouterModule.forChild(routes)
+    ],
+})
+export class AuthModule { }
---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required]),
+    });
+  }
+  onLoginSubmit() {
+    const email = this.loginForm.value.email;
+    const password = this.loginForm.value.password;
+
+    console.log(email, password)
+  }
+}
